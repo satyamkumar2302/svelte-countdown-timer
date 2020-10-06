bd119e2416cf1099272ce66d976d50679f18e161
satyam <satyamkumar2302@gmail.com>
2020-10-07 03:36:01 +0530

    completed adding all js functionality for timer

--- src/HowTo.svelte
+++ src/HowTo.svelte
@@ -1,3 +1,3 @@
 <div style={"max-width:100%"} bp="grid">
-    <img bp="offset-5@md 4@md 12@sm" src="handwashing.gif" alt="statue"/>
+    <img style={"max-width:100%"} bp="offset-5@md 4@md 12@sm" src="handwashing.gif" alt="statue"/>
 </div>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,5 +1,22 @@
 <script>
     import ProgressBar from "./ProgressBar.svelte";
+    let secondsleft = 20;
+    let isRunning = false;
+
+    const startTimer = ()=>{
+        const timer = setInterval(()=>{
+            secondsleft-=1;
+            isRunning=true;
+            if(secondsleft<=0) {
+                clearInterval(timer); 
+                secondsleft=20;
+                isRunning=false;
+            }
+        }, 1000);
+    }
+
+    $: progress = (20-secondsleft)/20*100;
+    
 </script>
 
 <style>
@@ -8,14 +25,19 @@
         width: 100%;
         margin: 10px 0;
     }
+
+    .start[disabled]{
+        background-color: rgb(194, 194, 194);
+        cursor: not-allowed;
+    }
 </style>
 
 <div bp="grid">
-    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: </h2>
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondsleft}</h2>
 </div>
 
-<ProgressBar/>
+<ProgressBar progress={progress}/>
 
 <div bp="grid">
-    <button class="start" bp="offset-5@md 4@md 12@sm">Start</button>
+    <button class="start" bp="offset-5@md 4@md 12@sm" disabled={isRunning} on:click={startTimer}>Start</button>
 </div>
