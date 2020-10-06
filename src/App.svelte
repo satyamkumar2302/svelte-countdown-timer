84f1e623ad55f6ae1818d5600480234de9730db5
satyam <satyamkumar2302@gmail.com>
2020-10-07 00:25:52 +0530

    html and css mockup done

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Timer from "./Timer.svelte";
+    import HowTo from "./HowTo.svelte";
+</script>
+
+<h1 style={"text-align:center"}>Countdown Timer for Handwash</h1>
+
+<Timer/>
+
+<HowTo/>
+
+<h3 style={"text-align:center"}>Powered by Svelte and Blueprint CSS</h3>
---
+++ src/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import ProgressBar from "./ProgressBar.svelte";
+</script>
+
+<style>
+    .start{
+        background-color: rgb(154,73,73);
+        width: 100%;
+        margin: 10px 0;
+    }
+</style>
+
+<div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: </h2>
+</div>
+
+<ProgressBar/>
+
+<button class="start">Start</button>
+
